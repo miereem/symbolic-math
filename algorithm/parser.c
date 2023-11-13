5255fac176bc4716f5b2605d107c43e5792ce646
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-11-13 18:38:08 +0300

    feat: base algoritm logic

--- algorithm/parser.c
+++ algorithm/parser.c
@@ -1,156 +1,159 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-// Типы токенов
-typedef enum {
+enum TokenType {
     NUMBER,
-    ADD,
-    SUBTRACT,
-    MULTIPLY,
-    DIVIDE,
-    LPAREN,
-    RPAREN,
-    END
-} TokenType;
+    OPERATOR,
+    VARIABLE
+};
 
-// Структура токена
-typedef struct {
-    TokenType type;
-    int value;  // Для NUMBER
-} Token;
 
-// Глобальная переменная для хранения текущего токена
-Token currentToken;
+struct ExpressionNode {
+    enum TokenType type;
+    // можно будет если нужно добавить тип опеарации пока хз зач
+    union {
+        int operand;        //  NUMBER nodes
+        char variable; // VARIABLE
+        char op;            //  OPERATOR nodes
+    };
+    struct ExpressionNode *left;
+    struct ExpressionNode *right;
+};
 
-// Прототипы функций
-void getNextToken();
-int parseExpression();
-int parsePrimary();
-int parseUnary();
-int parseNumber();
+int parseNumber(const char **expr);
+char parseOperator(const char **expr);
+struct ExpressionNode* parseExpression(const char **expr, int minPrecedence);
+struct ExpressionNode* parsePrimary(const char **expr);
 
-// Функция для вывода сообщения об ошибке
-void error(const char *message) {
-    fprintf(stderr, "%s\n", message);
-    exit(1);
-}
-
-// Функция для получения следующего токена из входной строки
-void getNextToken() {
-    char c = getchar();
-
-    while (isspace(c)) {
-        c = getchar();
-    }
-
-    if (isdigit(c)) {
-        ungetc(c, stdin);
-        scanf("%d", &currentToken.value);
-        currentToken.type = NUMBER;
-    } else {
-        switch (c) {
-            case '+':
-                currentToken.type = ADD;
-                break;
-            case '-':
-                currentToken.type = SUBTRACT;
-                break;
-            case '*':
-                currentToken.type = MULTIPLY;
-                break;
-            case '/':
-                currentToken.type = DIVIDE;
-                break;
-            case '(':
-                currentToken.type = LPAREN;
-                break;
-            case ')':
-                currentToken.type = RPAREN;
-                break;
-            case '\n':
-            case EOF:
-                currentToken.type = END;
-                break;
-            default:
-                error("Invalid character in input");
-        }
-    }
-    // printf("%d",(currentToken.value));
-    // printf("lala");
-    // printf("%c",(char) currentToken.type);
-}
-
-// Функция для парсинга выражения
-int parseExpression() {
-    int left = parseUnary();
-
-    while (currentToken.type == ADD || currentToken.type == SUBTRACT) {
-        Token op = currentToken;
-        getNextToken();
-        int right = parseUnary();
-        if (op.type == ADD) {
-            left += right;
-        } else {
-            left -= right;
-        }
-    }
-
-    return left;
-}
-
-// Функция для парсинга первичных выражений
-int parsePrimary() {
-    int result;
-
-    switch (currentToken.type) {
-        case NUMBER:
-            result = currentToken.value;
-            getNextToken();
-            break;
-        case LPAREN:
-            getNextToken();
-            result = parseExpression();
-            if (currentToken.type != RPAREN) {
-                error("Expected closing parenthesis");
-            }
-            getNextToken();
-            break;
+// Определяем приоритет
+int getPrecedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
         default:
-            error("Unexpected token in primary expression");
+            return 0;
     }
+}
 
+// строим число из его цифр ( типо распрастранненый метод)
+int parseNumber(const char **expr) {
+    int result = 0;
+    while (**expr >= '0' && **expr <= '9') {
+        result = result * 10 + (**expr - '0');
+        (*expr)++;
+    }
     return result;
 }
 
-// Функция для парсинга унарных выражений
-int parseUnary() {
-    if (currentToken.type == ADD || currentToken.type == SUBTRACT) {
-        Token op = currentToken;
-        getNextToken();
-        int operand = parseUnary();
-        if (op.type == SUBTRACT) {
-            return -operand;
+// Парсим оператора
+char parseOperator(const char **expr) {
+    char op = **expr;
+    (*expr)++;
+    return op;
+}
+
+char parseVariable(const char **expr) {
+    char var = **expr;
+    (*expr)++;
+    return var;
+}
+
+
+// Создаем ноду
+struct ExpressionNode* createNode(enum TokenType type, int operand, char variable, char op, struct ExpressionNode *left, struct ExpressionNode *right) {
+    struct ExpressionNode *node = (struct ExpressionNode *)malloc(sizeof(struct ExpressionNode));
+    if (!node) {
+        fprintf(stderr, "Error: Memory allocation failure\n");
+        exit(EXIT_FAILURE);
+    }
+
+    node->type = type;
+    if (type == NUMBER) {
+        node->operand = operand;
+    } else if (type == VARIABLE) {
+        node->variable = variable;
+    } else if (type == OPERATOR) {
+        node->op = op;
+    }
+    node->left = left;
+    node->right = right;
+
+    return node;
+}
+
+// Parse a primary expression (number or expression in parentheses)
+struct ExpressionNode* parsePrimary(const char **expr) {
+    if (**expr == '(') {
+        (*expr)++;
+        struct ExpressionNode *result = parseExpression(expr, 0);
+        if (**expr == ')') {
+            (*expr)++;
+        } else {
+            fprintf(stderr, "Error: Expected closing parenthesis\n");
+            exit(EXIT_FAILURE);
         }
-        return operand;
+        return result;
+    } else if (**expr >= 'a' && **expr <= 'z') {
+        char variable = parseVariable(expr);
+        return createNode(VARIABLE, 0, variable, 0, NULL, NULL);
     } else {
-        return parsePrimary();
+        int operand = parseNumber(expr);
+        return createNode(NUMBER, operand, 0,0, NULL, NULL);
+    }
+}
+
+// выражения
+struct ExpressionNode* parseExpression(const char **expr, int minPrecedence) {
+    struct ExpressionNode *leftOperand = parsePrimary(expr);
+
+    while (1) {
+        int currentPrecedence = getPrecedence(**expr);
+
+        // Оператор имеет приоритет ниже -
+        if (currentPrecedence <= minPrecedence) {
+            return leftOperand;
+        }
+
+        char op = parseOperator(expr);
+
+        //парсим правый операнд
+        struct ExpressionNode *rightOperand = parseExpression(expr, currentPrecedence);
+
+        // новая нода
+        leftOperand = createNode(OPERATOR, 0, 0, op, leftOperand, rightOperand);
+    }
+}
+
+//выводим приорите нод в нотации 
+void printPostfix(struct ExpressionNode *node) {
+    if (node) {
+        printPostfix(node->left);
+        printPostfix(node->right);
+        if (node->type == NUMBER) {
+            printf("%d ", node->operand);
+        } else if (node->type == VARIABLE) {
+            printf("%c ", node->variable);
+        } else if (node->type == OPERATOR) {
+            printf("%c ", node->op);
+        }
     }
 }
 
 int main() {
-    while (1) {
-        printf("Enter an expression: ");
-        getNextToken();
+    const char *input = "((3+c)*a)-7";
+    struct ExpressionNode *ast = parseExpression(&input, 0);
 
-        if (currentToken.type == END) {
-            break;
-        }
+    printf("Postfix Notation: ");
+    printPostfix(ast);
+    printf("\n");
 
-        int result = parseExpression();
 
-        printf("Result: %d\n", result);
-    }
+    // освобождаем память для нод
+    free(ast);
 
     return 0;
 }
