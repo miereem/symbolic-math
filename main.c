10e9d965e9c85650dbd797a3e6a5b64da3814384
blue <336767@niuitmo.ru>
2024-01-08 11:14:16 +0300

    separates

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-    char *input = "seq[set[f[0],0],set[f[1],1],set[f[2],1],setDelayed[f[n_],sum[f[sum[n,-1]],f[sum[n,-2]]]],f[20]]";
+    char *input = "seq[set[f[0],0],set[f[1],1],s e t[f[2],1],setDelayed[f[n_],sum[f[sum[n,-1]],f[sum[n,-2]]]],f[20]]";
 
     initContext();
     solve(&input);
