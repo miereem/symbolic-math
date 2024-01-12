304e74364517da97a6b649d96a7599a21a181a4d
miereem <mabdurasulkyzy@gmail.com>
2024-01-12 19:56:17 +0300

    derivative set fixed

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ int main() {
     char *If = "seq[setDelayed[if[true,t_,e_],t],setDelayed[if[false,t_,e_],e],addAttrs[if,holdRest]]";
     char *While = "setDelayed[f[x_],if[less[x,3],1,sum[f[sum[x,-1]],f[sum[x,-2]]]]]";
 
-    char *input = "numberQ[0]";
-    char *der4 = "seq[setDelayed[d[p_],if[numberQ[p],0,p]],set[d[sin[p_]],cos[p]]]";
+    char *input = "numberQ[sum[9,5]]";
+    char *der4 = "seq[setDelayed[d[p_],if[numberQ[p],0,1]],set[d[sin[p_]],cos[p]]]";
 
     char *der = "seq[set[d[sin[p_]],cos[p]]]";
     char *der0 = "seq[set[d[cos[p_]],mul[sin[p],-1]]]";
     char *der1 = "d[sin[x]]";
     char *der2 = "seq[setDelayed[d[sum[a_,b_]],sum[d[a],d[b]]]]";
-    char *der3 = "d[sum[sin[x],1]]";
+    char *der3 = "d[sum[sin[x],x]]";
 
 
     char *plotIn = "plot[s[l[p[1,2], p[2,7]], l[p[1,5],p[6,7]], l[p[2,3], p[6,27]]],400,500]";
